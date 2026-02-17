a07cd7b885c061d6acf129fc81f66c308c0629b2
Mahmoud Abo-Zied <116457362+EpitomeZied@users.noreply.github.com>
2026-02-17 20:57:45 +0200

    Time: 2 ms (69.3%), Space: 57.4 MB (8.3%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    const mp = new Map(); // val -> idx
+    for (let i = 0; i < nums.length; i++) {
+        if (mp.has(target - nums[i])) {
+            return [mp.get(target - nums[i]), i];
+        }
+        mp.set(nums[i], i);
+    }
+    return [];
+};
